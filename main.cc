d85a8faebc3b926dfdbfcd5c7df651ca106a5784
sajmon <szymon17099@gmail.com>
2023-05-07 01:47:45 +0200

    Changed the initial population size.

--- main.cc
+++ main.cc
@@ -2,7 +2,7 @@
 
 auto main() -> int {
 	Simulator program = Simulator::Builder()
-		.setInitialPopulation(50)
+		.setInitialPopulation(100)
 		.setMaxPopulation(1000)
 		.setSpawnRadius(25)
 		.build();
